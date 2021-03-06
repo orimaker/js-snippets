10fdb10e4556a2c15ad7c93da734f72ac1d1fd52
orimaker <80171498+orimaker@users.noreply.github.com>
2021-03-06 19:37:48 +0100

    Update youtube-transcript-extractor.js
    wider exported text for readibility

--- youtube-transcript-extractor.js
+++ youtube-transcript-extractor.js
@@ -1,10 +1,10 @@
-//Get the transcript from youtube videos and consolidate it to 
+////Get the transcript from youtube videos and consolidate it to 
 
 // include juery as it is not there 
 var script = document.createElement('script');script.src = "https://code.jquery.com/jquery-3.4.1.min.js";document.getElementsByTagName('head')[0].appendChild(script);
 
 //version 1 - log it into the console
 var text = ''; $('ytd-transcript-body-renderer').find('div[role="button"]').each((index,div)=>text+=div.textContent.trim() + '\n');text
+
 //version 2 - create new web page
-var text = ''; $('ytd-transcript-body-renderer').find('div[role="button"]').each((index,div)=>text+=div.textContent.trim() + '<br/>');text
-window.open().document.write(output);
+var text = ''; $('ytd-transcript-body-renderer').find('div[role="button"]').each((index,div)=>{text+=div.textContent.trim();text+=index%2?'<br/>':' '});window.open().document.write(text);
